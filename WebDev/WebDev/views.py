355ceaf5409cfbb85186dc85d64b2d8cb3264fde
Davide Bizzarri <bizzarridavide@outlook.com>
2014-07-02 15:39:49 +0200

    views.py, settings.py, urls.py

---
+++ WebDev/WebDev/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def home(request):
+    template = loader.get_template('login.html')
+    return HttpResponse(template)
