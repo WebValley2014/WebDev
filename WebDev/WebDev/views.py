b5bac55adfac0ee7be9da7506205ba6822ff11e6
Davide Bizzarri <bizzarridavide@outlook.com>
2014-07-03 15:58:31 +0200

    about_us

--- WebDev/WebDev/views.py
+++ WebDev/WebDev/views.py
@@ -1,51 +1,14 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import RequestContext, loader, Template, Context
-from django.contrib.auth import authenticate, login, logout
-from django.views.decorators.csrf import csrf_exempt, requires_csrf_token
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.shortcuts import render_to_response
-from forms import ContactForm, UploadFileForm
+from forms import UploadFileForm
 
 
 def index(request):
     return render(request, 'index.html')
 
-def logout_user(request):
-    logout(request)
-    return HttpResponseRedirect('/')
-
 def contact(request):
-    if request.method == 'POST': # If the form has been submitted...
-        # ContactForm was defined in the previous section
-        form = ContactForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            sender = form.cleaned_data['sender']
-            cc_myself = form.cleaned_data['cc_myself']
-            return HttpResponse(subject+"<br>"+message) # Redirect after POST
-    else:
-        form = ContactForm() # An unbound form
+    return render(request, 'contact.html')
 
-    return render(request, 'contact.html', {
-        'form': form,
-    })
-
-@login_required(login_url="/login")
-def upload(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            f = request.FILES['file']
-            name = f.name
-            pos = 'WebDev/uploadedFiles/'+name
-            with open(pos, 'wb+') as destination:
-                for chunk in f.chunks():
-                     destination.write(chunk)
-            return HttpResponse(name)
-    else:
-        form = UploadFileForm()
-    return render(request, 'upload.html', {
-        'form': form,
-        })
+def about_us(request):
+    return render(request, 'about_us.html')
