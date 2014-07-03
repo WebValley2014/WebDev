03833107e6143421ab26a8d2527e9b8defd3a27d
filosi <filosi@fbk.eu>
2014-07-03 09:24:54 +0200

    Added urls for each application

---
+++ WebDev/network/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+from .views import *
+admin.autodiscover()
+
+
+urlpatterns = patterns('network.views',
+                       )
