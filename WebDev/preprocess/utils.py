3c48111241e407846f2612fda4324a6eba70dc00
filosi <filosi@fbk.eu>
2014-07-03 22:17:07 +0200

    Parameters for upload files in preprocess

--- WebDev/preprocess/utils.py
+++ WebDev/preprocess/utils.py
@@ -1,5 +1,16 @@
+from django.conf import settings
+import os
+
+
 __author__ = 'filippo'
 def handle_uploaded_file(f):
+
+    upload_dir = date.today().strftime(settings.UPLOAD_PATH)
+    upload_full_path = os.path.join(settings.MEDIA_ROOT, upload_dir)
+
+    if not os.path.exists(upload_full_path):
+        os.makedirs(upload_full_path)
+
     with open('preprocess/uploadedFiles/'+f.name, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
