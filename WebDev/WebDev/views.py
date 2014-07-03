1953d5b6f0974508badf88c19d3ed226a16a3056
Davide Bizzarri <bizzarridavide@outlook.com>
2014-07-03 16:15:21 +0200

    alleggerito

--- WebDev/WebDev/views.py
+++ WebDev/WebDev/views.py
@@ -1,7 +1,4 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render
-from forms import UploadFileForm
 
 
 def index(request):
--- WebDev/preprocess/views.py
+++ WebDev/preprocess/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def preprocess(request):
+    return H
