a756d58dafd8f4165de5482fbba7e5867d3b7bff
Davide Bizzarri <bizzarridavide@outlook.com>
2014-07-03 09:49:39 +0200

    Insert the logout method and the login error

--- WebDev/WebDev/views.py
+++ WebDev/WebDev/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader, Template, Context
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt, requires_csrf_token
-#import django.contrib.auth.views.login
+from django.shortcuts import render
+from forms import ContactForm
 
 
 def index(request):
@@ -19,4 +20,22 @@ def logout_user(request):
 @login_required(login_url="/login")
 def upload(request):
     return HttpResponse("UPLOAD")
+
+
+def contact(request):
+    if request.method == 'POST': # If the form has been submitted...
+        # ContactForm was defined in the previous section
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['sender']
+            cc_myself = form.cleaned_data['cc_myself']
+            return HttpResponse(subject+"<br>"+message) # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    return render(request, 'contact.html', {
+        'form': form,
+    })
 
