19cbd15ed885c7ef53eda97a5594c0d4796e7393
Filippo Quattrocchi <filippo.quattrocchi@live.it>
2014-07-04 11:25:36 +0200

    classification: upload on the right path

--- WebDev/classification/views.py
+++ WebDev/classification/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from WebDev.models import *
+from django.utils import timezone
 # Create your views here.
 
 from forms import CLUploadFileForm
@@ -13,10 +15,12 @@ def classification(request, p_id = ''):
 
 @login_required(login_url="/login")
 def upload_preProcessed(request):
+    p = Pipeline(pip_name='Classification', pip_id='', started=timezone.now(), description='', owner=request.user)
+    p.save()
     form = CLUploadFileForm()
     if request.POST and request.FILES:
         form = CLUploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
+            handle_uploaded_file(request.user.username, p.pk, request.FILES['file'])
             return render(request, 'classification/tuttook.html')
     return render(request, 'classification/classification.html')
