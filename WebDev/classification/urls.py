77162b6af3489df0ac34dde7a27feeb553ec7a86
Filippo Quattrocchi <filippo.quattrocchi@live.it>
2014-07-07 11:32:31 +0200

    fix bugs

--- WebDev/classification/urls.py
+++ WebDev/classification/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('classification.views',
     #Upload Pre-Processed file
-    url(regex=r'^upload/$', view="upload_preProcessed", name="upload_preProcessed"),
+    url(regex=r'^upload/$', view="upload_preProcessed", name="class_upload"),
     #Download
     url(regex=r'^download/(.+)/$', view="download", name="download"),
     #Option-PreAnalytics
